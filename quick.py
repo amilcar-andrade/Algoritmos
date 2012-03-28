f0a48b4ae42aa5c88fea365aa9f93abd4fa26db8
Amilcar Andrade <amilcar.andrade.g@gmail.com>
2012-03-27 21:47:33 -0600

    quicksort

---
+++ quick.py
@@ -0,0 +1,49 @@
+
+
+
+def pivote(x):
+	izq = []
+	derecha = []
+		for i in range(0,len(a)):
+			if i< len(a)/2:
+				izq.append(a[i])
+			else:
+				derecha.append(a[i])
+		        
+	if len(derecha) <= 1:
+		return derecha
+	elif len(izq) <= 1:
+		return izq
+	elif len(derecha) > 0:
+		pivot = derecha[0]
+		i=0
+		j=0
+			for w in range(1,len(derecha)):
+    			if pivot > derecha[w]:
+        			i = i+1
+        			j = j+1
+        			derecha[i], derecha[j] = derecha[j], derecha[i]
+    			else:
+        			j=j+1
+					derecha.insert(i+1,pivot)
+					del derecha[0]
+	else:
+		pivot = izq[0]
+		i=0
+		j=0
+			for w in range(1,len(izq)):
+	    		if pivot > izq[w]:
+	        		i = i+1
+	        		j = j+1
+	        		izq[i], izq[j] = izq[j], izq[i]
+	    		else:
+	        		j=j+1
+					izq.insert(i+1,pivot)
+					del izq[0]
+	
+	final = izq + derecha
+	return izq
+		
+		
+
+
